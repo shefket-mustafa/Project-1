1878e1fb3d8d89e97283a7b78581109d7e930e4f
shefket-mustafa <miracle_1@abv.bg>
2025-04-07 13:55:45 +0300

    Home page created

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import './home.css'
+
+export default function Home() {
+  return (
+      <>
+
+      <section className="home-section">
+        <div className="welcoming-text">
+            Welcome to our <strong>Weathering</strong>
+        </div>
+
+        <div className="home-to-catalog">Forecast</div>
+        </section>
+      </>
+    );
+}
