cd1e9083cb3096aaa12a4964603c913386e4b2e5
shefket-mustafa <miracle_1@abv.bg>
2025-04-12 18:06:08 +0300

    error handling function added for error modal

--- src/components/context/UserProvider.jsx
+++ src/components/context/UserProvider.jsx
@@ -1,20 +1,43 @@
 import { useEffect, useState } from "react";
 import { UserContext } from "./userContext";
 import { requester } from "../../api/requester";
+import ErrorModal from "../error-modal/ErrorModal";
 
 export default function UserProvider({children}) {
 
     const [input, setInput] = useState('');
     const [data, setData] = useState({});
+    const [error, setError] = useState('');
     const [isSearchedClicked, setIsSearchClicked] = useState(false);
 
+
     useEffect(()=>{
-        if(isSearchedClicked){
-            const result = requester(input)
-            .then(result => setData(result));
-            setIsSearchClicked(false);
+        if(!isSearchedClicked){
+            return
+        };
+
+        try{
+                const result = requester(input)
+                .then(result => {
+                    if (!result || result.error) {
+                        errorHandler(result.error.message)
+                        throw new Error(result.error.message);
+                    }
+                    setData(result)
+                });
+                setIsSearchClicked(false);
+            
+        }catch(err){
+            errorHandler(err.message)
         };
     },[input,isSearchedClicked]);
+
+    const errorHandler = (message) => {
+        setError(message);
+        setTimeout(()=>{
+            setError('')
+        },2000);
+    };
 
     const inputHandler = (value) => {
         setInput(value)
@@ -22,15 +45,18 @@ export default function UserProvider({children}) {
 
     const searchHandler = () => {
         setIsSearchClicked(true);
+        setData({});
     };
 
   return (
       <UserContext.Provider value={{
+        errorHandler,
         inputHandler,
         searchHandler,
         data,
       }}>
 
+        {error && <ErrorModal message ={error}/>}
         {children}
         </UserContext.Provider>
     );
